c85ca7571803fd0b9f6e7967f3af2272e03ff203
Alexandr <erashov@outlook.com>
2015-10-02 16:25:27 +0300

    Change

--- Navigator.WebUI/Controllers/GridController.cs
+++ Navigator.WebUI/Controllers/GridController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 
 namespace Navigator.WebUI.Controllers
@@ -16,9 +17,12 @@ namespace Navigator.WebUI.Controllers
         {
             this._repository = routesheetRepository;
         }
+        HttpRequest _request = HttpContext.Current.Request;
         public IQueryable<ListMLGrid> GetListMLGrids()
         {
-            return _repository.ListMLGrids.AsQueryable();
+            int take = _request["take"]==null?10:int.Parse(_request["take"]);
+            int skip = _request["skip"] == null ? 0 : int.Parse(_request["skip"]);
+            return _repository.ListMLGrids;//.Skip(skip).Take(take);//.qu.OrderByDescending(c=>c.NumML).AsQueryable();
         }
     }
 }
---
+++ Navigator.WebUI/Models/KendoResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Navigator.WebUI.Models
+{
+    public class KendoResponse
+    {
+        public Array Data { get; set; }
+        public int Count { get; set; }
+        public string Errors { get; set; }
+
+        public KendoResponse(Array data, int count)
+        {
+            this.Data = data;
+            this.Count = count;
+        }
+
+        public KendoResponse(string errors)
+        {
+            this.Errors = errors;
+        }
+
+        public KendoResponse()
+        {
+
+        }
+    }
+}
