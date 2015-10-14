6c704f654c14b97780dea37b096db0939b9ff5d6
Alexandr <erashov@outlook.com>
2015-10-14 16:09:49 +0300

    change

--- Navigator.Web/Controllers/ListMLsController.cs
+++ Navigator.Web/Controllers/ListMLsController.cs
@@ -1,7 +1,11 @@
 ﻿using Kendo.Mvc.Extensions;
 using Navigator.Domain.Abstract;
-using Navigator.Web.KendoGridUtilities;
+using Navigator.Domain.Entities;
+using Navigator.Web.Models;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Dynamic;
 using System.Web.Mvc;
 
 namespace Navigator.Web.Controllers
@@ -18,28 +22,123 @@ namespace Navigator.Web.Controllers
         {
             return View();
         }
-        public ActionResult GetListMls(int page, int pageSize, int take)
+        public ActionResult GetListMls(int page, int pageSize, int take, List<GridSort> sort = null, GridFilters filter = null)
         {
-            var sorterCollection = KendoGridSorterCollection.BuildCollection(Request);
-            var filterCollection = KendoGridFilterCollection.BuildCollection(Request);
-            var filteredMls = _repository.ListMLGrids.MultipleFilter(filterCollection.Filters);
-            var sortedMls = filteredMls;
-            if (sorterCollection.Sorters.Count() == 0)
+            var mls = _repository.ListMLGrids.AsQueryable();
+            var totalCount = _repository.ListMLGrids.Count();
+
+            if (filter != null && (filter.Filters != null && filter.Filters.Count > 0))
             {
-                sortedMls = filteredMls.OrderByDescending(v => v.NumML);
+                string whereClause = null;
+                var parameters = new List<object>();
+                var filters = filter.Filters;
+
+                for (var i = 0; i < filters.Count; i++)
+                {
+                    if (i == 0)
+                        whereClause += string.Format(" {0}",
+                            BuildWhereClause<ListMLGrid>(i, filter.Logic, filters[i],
+                            parameters));
+                    else
+                        whereClause += string.Format(" {0} {1}",
+                            ToLinqOperator(filter.Logic),
+                            BuildWhereClause<ListMLGrid>(i, filter.Logic, filters[i],
+                            parameters));
+                }
+
+                mls = mls.Where(whereClause, parameters.ToArray());
+                totalCount = mls.Where(whereClause, parameters.ToArray()).Count();
+            }
+
+            if (sort != null && sort.Count > 0)
+            {
+                foreach (var s in sort)
+                {
+                    mls = mls.OrderBy(s.Field + " " + s.Dir);
+                }
             }
             else
             {
-                sortedMls = filteredMls.MultipleSort(sorterCollection.Sorters);
+                mls = mls.OrderBy("NumML desc");
             }
-            var count = sortedMls.Count();
-          //  var data = sortedMls.Skip((page - 1) * pageSize).Take(pageSize).ToList();
-            var data = (from v in sortedMls.Skip((page - 1) * pageSize)
-                            .Take(pageSize)
-                        select v).ToList();
-            var jsonData = new { total = count, data };
+            var data = mls.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+            var jsonData = new { total = totalCount, data = data };
             return Json(jsonData, JsonRequestBehavior.AllowGet);
+
+
+
+        }
+        public static string BuildWhereClause<T>(int index, string logic,
+        GridFilter filter, List<object> parameters)
+        {
+            var entityType = (typeof(T));
+            var property = entityType.GetProperty(filter.Field);
+
+            switch (filter.Operator.ToLower())
+            {
+                case "eq":
+                case "neq":
+                case "gte":
+                case "gt":
+                case "lte":
+                case "lt":
+                    if (typeof(DateTime).IsAssignableFrom(property.PropertyType))
+                    {
+                        parameters.Add(DateTime.Parse(filter.Value).Date);
+                        return string.Format("EntityFunctions.TruncateTime({0}){1}@{2}",
+                            filter.Field,
+                            ToLinqOperator(filter.Operator),
+                            index);
+                    }
+                    if (typeof(int).IsAssignableFrom(property.PropertyType))
+                    {
+                        parameters.Add(int.Parse(filter.Value));
+                        return string.Format("{0}{1}@{2}",
+                            filter.Field,
+                            ToLinqOperator(filter.Operator),
+                            index);
+                    }
+                    parameters.Add(filter.Value);
+                    return string.Format("{0}{1}@{2}",
+                        filter.Field,
+                        ToLinqOperator(filter.Operator),
+                        index);
+                case "startswith":
+                    parameters.Add(filter.Value);
+                    return string.Format("{0}.StartsWith(" + "@{1})",
+                        filter.Field,
+                        index);
+                case "endswith":
+                    parameters.Add(filter.Value);
+                    return string.Format("{0}.EndsWith(" + "@{1})",
+                        filter.Field,
+                        index);
+                case "contains":
+                    parameters.Add(filter.Value);
+                    return string.Format("{0}.Contains(" + "@{1})",
+                        filter.Field,
+                        index);
+                default:
+                    throw new ArgumentException(
+                        "This operator is not yet supported for this Grid",
+                        filter.Operator);
+            }
+        }
+
+        public static string ToLinqOperator(string @operator)
+        {
+            switch (@operator.ToLower())
+            {
+                case "eq": return " == ";
+                case "neq": return " != ";
+                case "gte": return " >= ";
+                case "gt": return " > ";
+                case "lte": return " <= ";
+                case "lt": return " < ";
+                case "or": return " || ";
+                case "and": return " && ";
+                default: return null;
+            }
         }
     }
-
 }
