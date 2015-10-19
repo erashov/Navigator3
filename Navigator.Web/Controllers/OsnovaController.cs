d0b74e24689d4e91339abfe09d52ef908e238459
Alexandr <erashov@outlook.com>
2015-10-19 18:02:15 +0300

    Update

---
+++ Navigator.Web/Controllers/OsnovaController.cs
@@ -0,0 +1,145 @@
+﻿using Navigator.Domain.Abstract;
+using Navigator.Domain.Entities;
+using Navigator.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic;
+using System.Web.Mvc;
+
+namespace Navigator.Web.Controllers
+{
+    public class OsnovaController : Controller
+    {
+        private IOsnovaRepository _repository;
+        public OsnovaController(IOsnovaRepository routesheetRepository)
+        {
+            this._repository = routesheetRepository;
+        }
+        // GET: Osnova
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetList(int page, int pageSize, int take,  List<GridSort> sort = null, GridFilters filter = null)
+        {
+            var osnovas = _repository.Osnovas.AsQueryable();
+            var totalCount = _repository.Osnovas.Count();
+
+            if (filter != null && (filter.Filters != null && filter.Filters.Count > 0))
+            {
+                string whereClause = null;
+                var parameters = new List<object>();
+                var filters = filter.Filters;
+
+                for (var i = 0; i < filters.Count; i++)
+                {
+                    if (i == 0)
+                        whereClause += string.Format(" {0}",
+                            BuildWhereClause<Osnova>(i, filter.Logic, filters[i],
+                            parameters));
+                    else
+                        whereClause += string.Format(" {0} {1}",
+                            ToLinqOperator(filter.Logic),
+                            BuildWhereClause<Osnova>(i, filter.Logic, filters[i],
+                            parameters));
+                }
+
+                osnovas = osnovas.Where(whereClause, parameters.ToArray());
+                totalCount = osnovas.Where(whereClause, parameters.ToArray()).Count();
+            }
+
+            if (sort != null && sort.Count > 0)
+            {
+                foreach (var s in sort)
+                {
+                  //  s.Field = (s.Field == "ProductType") ? "ProductTypeId" : s.Field;
+                    osnovas = osnovas.OrderBy(s.Field + " " + s.Dir);
+                }
+            }
+            else
+            {
+                osnovas = osnovas.OrderBy("NumML desc");
+            }
+            var data  = osnovas.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+            var jsonData = new { total = totalCount, data=data };
+            return Json(jsonData, JsonRequestBehavior.AllowGet);
+
+
+
+        }
+        public static string BuildWhereClause<T>(int index, string logic,
+        GridFilter filter, List<object> parameters)
+        {
+            var entityType = (typeof(T));
+            var property = entityType.GetProperty(filter.Field);
+
+            switch (filter.Operator.ToLower())
+            {
+                case "eq":
+                case "neq":
+                case "gte":
+                case "gt":
+                case "lte":
+                case "lt":
+                    if (typeof(DateTime).IsAssignableFrom(property.PropertyType))
+                    {
+                        parameters.Add(DateTime.Parse(filter.Value).Date);
+                        return string.Format("EntityFunctions.TruncateTime({0}){1}@{2}",
+                            filter.Field,
+                            ToLinqOperator(filter.Operator),
+                            index);
+                    }
+                    if (typeof(int).IsAssignableFrom(property.PropertyType))
+                    {
+                        parameters.Add(int.Parse(filter.Value));
+                        return string.Format("{0}{1}@{2}",
+                            filter.Field,
+                            ToLinqOperator(filter.Operator),
+                            index);
+                    }
+                    parameters.Add(filter.Value);
+                    return string.Format("{0}{1}@{2}",
+                        filter.Field,
+                        ToLinqOperator(filter.Operator),
+                        index);
+                case "startswith":
+                    parameters.Add(filter.Value);
+                    return string.Format("{0}.StartsWith(" + "@{1})",
+                        filter.Field,
+                        index);
+                case "endswith":
+                    parameters.Add(filter.Value);
+                    return string.Format("{0}.EndsWith(" + "@{1})",
+                        filter.Field,
+                        index);
+                case "contains":
+                    parameters.Add(filter.Value);
+                    return string.Format("{0}.Contains(" + "@{1})",
+                        filter.Field,
+                        index);
+                default:
+                    throw new ArgumentException(
+                        "This operator is not yet supported for this Grid",
+                        filter.Operator);
+            }
+        }
+
+        public static string ToLinqOperator(string @operator)
+        {
+            switch (@operator.ToLower())
+            {
+                case "eq": return " == ";
+                case "neq": return " != ";
+                case "gte": return " >= ";
+                case "gt": return " > ";
+                case "lte": return " <= ";
+                case "lt": return " < ";
+                case "or": return " || ";
+                case "and": return " && ";
+                default: return null;
+            }
+        }
+    }
+}
--- Navigator.Web/Scripts/gridMls.js
+++ Navigator.Web/Scripts/gridMls.js
@@ -5,6 +5,46 @@
     ],
     click: onClick
 });
+
+var columns = [
+        { width: 30, headerTemplate: "<input  type='checkbox' id='chkSelectAll' onclick='checkAll(this)' />", template: "<input  type='checkbox' class='chkbx'/>" },
+        { field: "NumML", title: "№ МЛ", template: "<a href='' class='redlink'>#=NumML# </a><div class=\"valueDel\"></div> #= utverzh ? kendo.toString(utverzh, 'dd.MM.yyyy') : '' #", width: 150 },
+        { field: "USERs", title: "Пользователь", width: 160, filterable: { cell: { operator: "contains" } } },
+        { field: "Otvetstv", title: "Ответственный", width: 160 },
+        { field: "Zakazhcik", title: "Заказчик", width: 190 },
+        { field: "AdresA", title: "Адрес", template: "#=AdresA ? AdresA:'' # <div class=\"valueDel\"></div> #=AdresB ? AdresB:''#", width: 400 },
+        { field: "AdressA", title: "Адрес СО", template: "#=AdressA ? AdressA:'' # <div class=\"valueDel\"></div> #=AdressB ? AdressB:''#", width: 400 },
+        { title: "Услуга", field: "Usluga", template: "<table><tr><td>#=Usluga ? Usluga:'' #  &nbsp;</td></tr><tr><td>#=Skorost ? Skorost:'' #  &nbsp; </td></tr><tr><td></td></tr></table>", width: 100 },
+        { field: "Styk_A", title: "Стык", template: "#=Styk_A ? Styk_A:'' # <div class=\"valueDel\"></div> #=Styk_B ? Styk_B:''#", width: 100 },
+        { field: "PrivA", title: "Узел привязки", template: "#=PrivA ? PrivA:'' # <div class=\"valueDel\"></div> #=PrivB ? PrivB:''#", width: 100 },
+        { field: "UDA", title: "Узел доступа", template: "#=UDA ? UDA:'' # <div class=\"valueDel\"></div> #=UDB ? UDB:''#", width: 100 },
+        { field: "SpecOb", title: "Канал привязки", width: 120 },
+        { field: "ChNumA", title: "F стыка", template: "#=ChNumA ? ChNumA:'' # <div class=\"valueDel\"></div> #=ChNumB ? ChNumB:''#", width: 100 },
+        { field: "ESVTPIQQCNfe", title: "E,S,V,T,P,I,Q,QC,N,Ф", width: 100 },
+        { field: "L", width: 100 }, { field: "E", width: 100 }, { field: "S", width: 100 }, { field: "V", width: 100 }, { field: "T", width: 100 },
+        { field: "P", width: 100 }, { field: "I", width: 100 }, { field: "Q", width: 100 }, { field: "QC", width: 100 }, { field: "N", width: 100 },
+        { field: "Fe", width: 100, title: "Ф" },
+        { field: "gplr_start", title: "ГПЛР", template: "<table><tr><td >#=gplr_start ? kendo.toString(gplr_start,'dd.MM.yyyy'):'' #</td><td>#=gplr2_start ? kendo.toString(gplr2_start,'dd.MM.yyyy'):'' #</td></tr><tr><td>#=gplr_end ? kendo.toString(gplr_end,'dd.MM.yyyy'):'' # #=gplrIsCanceled ? gplrIsCanceled:'' #</td><td>#=gplr2_end ? kendo.toString(gplr2_end,'dd.MM.yyyy'):'' # #=gplr2IsCanceled ? gplr2IsCanceled:''#</td></tr></table>", width: 160 },
+        { field: "dmv_start", title: "ДМВ", template: "<table><tr><td >#=dmv_start ? kendo.toString(dmv_start,'dd.MM.yyyy'):'' #</td><td>#=dmv2_start ? kendo.toString(dmv2_start,'dd.MM.yyyy'):'' #</td></tr><tr><td>#=dmv_end ? kendo.toString(dmv_end,'dd.MM.yyyy'):'' # #=dmvIsCanceled ? dmvIsCanceled:'' #</td><td>#=dmv2_end ? kendo.toString(dmv2_end,'dd.MM.yyyy'):'' #  #=dmv2IsCanceled ? dmv2IsCanceled:''#</td></tr></table>", width: 160 },
+        { field: "osp_start", title: "ОСП", template: "#=osp_start ? kendo.toString(osp_start,'dd.MM.yyyy'):'' #<div class=\"valueDel\"></div>#=osp_end ? kendo.toString(osp_end,'dd.MM.yyyy'):'' # #=ospIsCanceled ? ospIsCanceled:'' #", width: 160 },
+        { field: "uiias_h_start", title: "ОЦОДиТ", template: "#=uiias_h_start ? kendo.toString(uiias_h_start,'dd.MM.yyyy'):'' #<div class=\"valueDel\"></div>#=uiias_h_end ? kendo.toString(uiias_h_end,'dd.MM.yyyy'):'' # #=uiias_hIsCanceled ? uiias_hIsCanceled:'' #", width: 160 },
+        { field: "oshugpu_start", title: "ОШУГПУ", template: "#=oshugpu_start ? kendo.toString(oshugpu_start,'dd.MM.yyyy'):'' #<div class=\"valueDel\"></div>#=oshugpu_end ? kendo.toString(oshugpu_end,'dd.MM.yyyy'):'' # #=oshugpuIsCanceled ? oshugpuIsCanceled:'' #", width: 160 },
+        { field: "oshugpz_start", title: "ГФЗ", template: "#=oshugpz_start ? kendo.toString(oshugpz_start,'dd.MM.yyyy'):'' #<div class=\"valueDel\"></div>#=oshugpz_end ? kendo.toString(oshugpz_end,'dd.MM.yyyy'):'' # #=oshugpzIsCanceled ? oshugpzIsCanceled:'' #", width: 160 },
+        { field: "to_start", title: "ТО", template: "<table><tr><td >#=to_start ? kendo.toString(to_start,'dd.MM.yyyy'):''#</td><td>#=to2_start ? kendo.toString(to2_start,'dd.MM.yyyy'):''#</td></tr><tr><td>#=to_end ? kendo.toString(to_end,'dd.MM.yyyy'):''# #=toIsCanceled ? toIsCanceled:''#</td><td>#=to2_end ? kendo.toString(to2_end,'dd.MM.yyyy'):''# #=to2IsCanceled ? to2IsCanceled:''#</td></tr></table>", width: 160 },
+        { field: "otse_start", title: "ОТС(Е)", template: "<table><tr><td >#=otse_start ? kendo.toString(otse_start,'dd.MM.yyyy'):''#</td><td>#=otse2_start ? kendo.toString(otse2_start,'dd.MM.yyyy'):''#</td></tr><tr><td>#=otse_end ? kendo.toString(otse_end,'dd.MM.yyyy'):''# #=otseIsCanceled ? otseIsCanceled:''#</td><td>#=otse2_end ? kendo.toString(otse2_end,'dd.MM.yyyy'):''# #=otse2IsCanceled ? otse2IsCanceled:''#</td></tr></table>", width: 190 },
+        { field: "otss_start", title: "ОТС(S)", template: "<table><tr><td >#=otss_start ? kendo.toString(otss_start,'dd.MM.yyyy'):''#</td><td>#=otss2_start ? kendo.toString(otss2_start,'dd.MM.yyyy'):''#</td></tr><tr><td>#=otss_end ? kendo.toString(otss_end,'dd.MM.yyyy'):''# #=otssIsCanceled ? otssIsCanceled:''#</td><td>#=otss2_end ? kendo.toString(otss2_end,'dd.MM.yyyy'):''# #=otss2IsCanceled ? otss2IsCanceled:''#</td></tr></table>", width: 190 },
+        { field: "otu_start", title: "ОТУ", template: "#=otu_start ? kendo.toString(otu_start,'dd.MM.yyyy'):'' #<div class=\"valueDel\"></div>#=otu_end ? kendo.toString(otu_end,'dd.MM.yyyy'):'' # #=otuIsCanceled ? otuIsCanceled:'' #", width: 190 },
+        { field: "otvu_start", title: "ОТвУ", template: "#=otvu_start ? kendo.toString(otvu_start,'dd.MM.yyyy'):'' #<div class=\"valueDel\"></div>#=otvu_end ? kendo.toString(otvu_end,'dd.MM.yyyy'):'' # #=otvuIsCanceled ? otvuIsCanceled:'' #", width: 190 },
+        { field: "TaskReturns", title: "Возврат МЛ", width: 120 },
+        { field: "TaskCancels", title: "Возврат заданий", width: 120 },
+        { field: "StartDate", title: "Заморозка", template: "#=StartDate ? kendo.toString(StartDate,'dd.MM.yyyy'):'' # <div class=\"valueDel\"></div> #=StopDate ? kendo.toString(StopDate,'dd.MM.yyyy'):''#", width: 160 },
+        { field: "Probl", title: "Проблема", width: 130 },
+        { field: "Prim", title: "Примечание", width: 130 },
+        { field: "Operator", title: "Наименование СО", width: 180 },
+        { field: "test_date_a", title: "Тест СО", width: 190, template: "<table><tr><td>#=test_date_a ? kendo.toString(test_date_a,'dd.MM.yyyy'):''#</td><td>#=test_res_a ? test_res_a:''#&nbsp;</td></tr><tr><td>#=test_date_b ? kendo.toString(test_date_b,'dd.MM.yyyy'):''#&nbsp;</td><td>#=test_res_b ? test_res_a:''#&nbsp;</td></tr></table>" },
+        { field: "SpecProject", title: "Спецпроект", width: 130 }
+];
+
 $("#gridMls").kendoGrid({
     dataSource: {
         transport: {
@@ -73,44 +113,7 @@ $("#gridMls").kendoGrid({
         pageSizes: [10, 20, 50],
         buttonCount: 5
     },
-    columns: [
-        { width: 30, headerTemplate: "<input  type='checkbox' id='chkSelectAll' onclick='checkAll(this)' />", template: "<input  type='checkbox' class='chkbx'/>" },
-        { field: "NumML", title: "№ МЛ", template: "<a href='' class='redlink'>#=NumML# </a><div class=\"valueDel\"></div> #= utverzh ? kendo.toString(utverzh, 'dd.MM.yyyy') : '' #", width: 150 },
-        { field: "USERs", title: "Пользователь", width: 160, filterable: { cell: { operator: "contains" } } },
-        { field: "Otvetstv", title: "Ответственный", width: 160 },
-        { field: "Zakazhcik", title: "Заказчик", width: 190 },
-        { field: "AdresA", title: "Адрес", template: "#=AdresA ? AdresA:'' # <div class=\"valueDel\"></div> #=AdresB ? AdresB:''#", width: 400 },
-        { field: "AdressA", title: "Адрес СО", template: "#=AdressA ? AdressA:'' # <div class=\"valueDel\"></div> #=AdressB ? AdressB:''#", width: 400 },
-        { title: "Услуга", field: "Usluga", template: "<table><tr><td>#=Usluga ? Usluga:'' #  &nbsp;</td></tr><tr><td>#=Skorost ? Skorost:'' #  &nbsp; </td></tr><tr><td></td></tr></table>", width: 100 },
-        { field: "Styk_A", title: "Стык", template: "#=Styk_A ? Styk_A:'' # <div class=\"valueDel\"></div> #=Styk_B ? Styk_B:''#", width: 100 },
-        { field: "PrivA", title: "Узел привязки", template: "#=PrivA ? PrivA:'' # <div class=\"valueDel\"></div> #=PrivB ? PrivB:''#", width: 100 },
-        { field: "UDA", title: "Узел доступа", template: "#=UDA ? UDA:'' # <div class=\"valueDel\"></div> #=UDB ? UDB:''#", width: 100 },
-        { field: "SpecOb", title: "Канал привязки", width: 120 },
-        { field: "ChNumA", title: "F стыка", template: "#=ChNumA ? ChNumA:'' # <div class=\"valueDel\"></div> #=ChNumB ? ChNumB:''#", width: 100 },
-        { field: "ESVTPIQQCNfe", title: "E,S,V,T,P,I,Q,QC,N,Ф", width: 100 },
-        { field: "L", width: 100 }, { field: "E", width: 100 }, { field: "S", width: 100 }, { field: "V", width: 100 }, { field: "T", width: 100 },
-        { field: "P", width: 100 }, { field: "I", width: 100 }, { field: "Q", width: 100 }, { field: "QC", width: 100 }, { field: "N", width: 100 },
-        { field: "Fe", width: 100, title: "Ф" },
-        { field: "gplr_start", title: "ГПЛР", template: "<table><tr><td >#=gplr_start ? kendo.toString(gplr_start,'dd.MM.yyyy'):'' #</td><td>#=gplr2_start ? kendo.toString(gplr2_start,'dd.MM.yyyy'):'' #</td></tr><tr><td>#=gplr_end ? kendo.toString(gplr_end,'dd.MM.yyyy'):'' # #=gplrIsCanceled ? gplrIsCanceled:'' #</td><td>#=gplr2_end ? kendo.toString(gplr2_end,'dd.MM.yyyy'):'' # #=gplr2IsCanceled ? gplr2IsCanceled:''#</td></tr></table>", width: 160 },
-        { field: "dmv_start", title: "ДМВ", template: "<table><tr><td >#=dmv_start ? kendo.toString(dmv_start,'dd.MM.yyyy'):'' #</td><td>#=dmv2_start ? kendo.toString(dmv2_start,'dd.MM.yyyy'):'' #</td></tr><tr><td>#=dmv_end ? kendo.toString(dmv_end,'dd.MM.yyyy'):'' # #=dmvIsCanceled ? dmvIsCanceled:'' #</td><td>#=dmv2_end ? kendo.toString(dmv2_end,'dd.MM.yyyy'):'' #  #=dmv2IsCanceled ? dmv2IsCanceled:''#</td></tr></table>", width: 160 },
-        { field: "osp_start", title: "ОСП", template: "#=osp_start ? kendo.toString(osp_start,'dd.MM.yyyy'):'' #<div class=\"valueDel\"></div>#=osp_end ? kendo.toString(osp_end,'dd.MM.yyyy'):'' # #=ospIsCanceled ? ospIsCanceled:'' #", width: 160 },
-        { field: "uiias_h_start", title: "ОЦОДиТ", template: "#=uiias_h_start ? kendo.toString(uiias_h_start,'dd.MM.yyyy'):'' #<div class=\"valueDel\"></div>#=uiias_h_end ? kendo.toString(uiias_h_end,'dd.MM.yyyy'):'' # #=uiias_hIsCanceled ? uiias_hIsCanceled:'' #", width: 160 },
-        { field: "oshugpu_start", title: "ОШУГПУ", template: "#=oshugpu_start ? kendo.toString(oshugpu_start,'dd.MM.yyyy'):'' #<div class=\"valueDel\"></div>#=oshugpu_end ? kendo.toString(oshugpu_end,'dd.MM.yyyy'):'' # #=oshugpuIsCanceled ? oshugpuIsCanceled:'' #", width: 160 },
-        { field: "oshugpz_start", title: "ГФЗ", template: "#=oshugpz_start ? kendo.toString(oshugpz_start,'dd.MM.yyyy'):'' #<div class=\"valueDel\"></div>#=oshugpz_end ? kendo.toString(oshugpz_end,'dd.MM.yyyy'):'' # #=oshugpzIsCanceled ? oshugpzIsCanceled:'' #", width: 160 },
-        { field: "to_start", title: "ТО", template: "<table><tr><td >#=to_start ? kendo.toString(to_start,'dd.MM.yyyy'):''#</td><td>#=to2_start ? kendo.toString(to2_start,'dd.MM.yyyy'):''#</td></tr><tr><td>#=to_end ? kendo.toString(to_end,'dd.MM.yyyy'):''# #=toIsCanceled ? toIsCanceled:''#</td><td>#=to2_end ? kendo.toString(to2_end,'dd.MM.yyyy'):''# #=to2IsCanceled ? to2IsCanceled:''#</td></tr></table>", width: 160 },
-        { field: "otse_start", title: "ОТС(Е)", template: "<table><tr><td >#=otse_start ? kendo.toString(otse_start,'dd.MM.yyyy'):''#</td><td>#=otse2_start ? kendo.toString(otse2_start,'dd.MM.yyyy'):''#</td></tr><tr><td>#=otse_end ? kendo.toString(otse_end,'dd.MM.yyyy'):''# #=otseIsCanceled ? otseIsCanceled:''#</td><td>#=otse2_end ? kendo.toString(otse2_end,'dd.MM.yyyy'):''# #=otse2IsCanceled ? otse2IsCanceled:''#</td></tr></table>", width: 190 },
-        { field: "otss_start", title: "ОТС(S)", template: "<table><tr><td >#=otss_start ? kendo.toString(otss_start,'dd.MM.yyyy'):''#</td><td>#=otss2_start ? kendo.toString(otss2_start,'dd.MM.yyyy'):''#</td></tr><tr><td>#=otss_end ? kendo.toString(otss_end,'dd.MM.yyyy'):''# #=otssIsCanceled ? otssIsCanceled:''#</td><td>#=otss2_end ? kendo.toString(otss2_end,'dd.MM.yyyy'):''# #=otss2IsCanceled ? otss2IsCanceled:''#</td></tr></table>", width: 190 },
-        { field: "otu_start", title: "ОТУ", template: "#=otu_start ? kendo.toString(otu_start,'dd.MM.yyyy'):'' #<div class=\"valueDel\"></div>#=otu_end ? kendo.toString(otu_end,'dd.MM.yyyy'):'' # #=otuIsCanceled ? otuIsCanceled:'' #", width: 190 },
-        { field: "otvu_start", title: "ОТвУ", template: "#=otvu_start ? kendo.toString(otvu_start,'dd.MM.yyyy'):'' #<div class=\"valueDel\"></div>#=otvu_end ? kendo.toString(otvu_end,'dd.MM.yyyy'):'' # #=otvuIsCanceled ? otvuIsCanceled:'' #", width: 190 },
-        { field: "TaskReturns", title: "Возврат МЛ", width: 120 },
-        { field: "TaskCancels", title: "Возврат заданий", width: 120 },
-        { field: "StartDate", title: "Заморозка", template: "#=StartDate ? kendo.toString(StartDate,'dd.MM.yyyy'):'' # <div class=\"valueDel\"></div> #=StopDate ? kendo.toString(StopDate,'dd.MM.yyyy'):''#", width: 160 },
-        { field: "Probl", title: "Проблема", width: 130 },
-        { field: "Prim", title: "Примечание", width: 130 },
-        { field: "Operator", title: "Наименование СО", width: 180 },
-        { field: "test_date_a", title: "Тест СО", width: 190, template: "<table><tr><td>#=test_date_a ? kendo.toString(test_date_a,'dd.MM.yyyy'):''#</td><td>#=test_res_a ? test_res_a:''#&nbsp;</td></tr><tr><td>#=test_date_b ? kendo.toString(test_date_b,'dd.MM.yyyy'):''#&nbsp;</td><td>#=test_res_b ? test_res_a:''#&nbsp;</td></tr></table>" },
-        { field: "SpecProject", title: "Спецпроект", width: 130 }
-    ]
+    columns: columns
 });
 
 function onClick(e) {
