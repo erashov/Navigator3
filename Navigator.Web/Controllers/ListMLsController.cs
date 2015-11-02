c743e1225c775f4b9d86e797d23faad6ab671601
Alexandr <erashov@outlook.com>
2015-11-02 09:33:00 +0300

    Update

--- Navigator.Web/Controllers/ListMLsController.cs
+++ Navigator.Web/Controllers/ListMLsController.cs
@@ -28,26 +28,18 @@ namespace Navigator.Web.Controllers
             {
                 var mls = _repository.RouteSheets.AsQueryable();
                 var totalCount = _repository.RouteSheets.Count();
-
                 if (filter != null && (filter.Filters != null && filter.Filters.Count > 0))
                 {
                     string whereClause = null;
                     var parameters = new List<object>();
                     var filters = filter.Filters;
-
                     for (var i = 0; i < filters.Count; i++)
                     {
                         if (i == 0)
-                            whereClause += string.Format(" {0}",
-                                BuildWhereClause<ListMLGrid>(i, filter.Logic, filters[i],
-                                parameters));
+                            whereClause += string.Format(" {0}", BuildWhereClause<ListMLGrid>(i, filter.Logic, filters[i], parameters));
                         else
-                            whereClause += string.Format(" {0} {1}",
-                                ToLinqOperator(filter.Logic),
-                                BuildWhereClause<ListMLGrid>(i, filter.Logic, filters[i],
-                                parameters));
+                            whereClause += string.Format(" {0} {1}", ToLinqOperator(filter.Logic), BuildWhereClause<ListMLGrid>(i, filter.Logic, filters[i], parameters));
                     }
-
                     mls = mls.Where(whereClause, parameters.ToArray());
                     totalCount = mls.Where(whereClause, parameters.ToArray()).Count();
                 }
@@ -86,53 +78,31 @@ namespace Navigator.Web.Controllers
                 case "gt":
                 case "lte":
                 case "lt":
-                    if ( filter.Field == "oshugpz_start")
+                    if (filter.Field == "oshugpz_start" || filter.Field == "oshugpz_end")
                     {
                         parameters.Add(DateTime.Parse(filter.Value).Date);
-
-                        return string.Format("{0}{1}@{2}",
-                filter.Field,
-                ToLinqOperator(filter.Operator),
-                index);
-                        //return string.Format("EntityFunctions.TruncateTime({0}){1}@{2}",
-                        //    filter.Field,
-                        //    ToLinqOperator(filter.Operator),
-                        //    index);
+                        return string.Format("{0}{1}@{2}", filter.Field, ToLinqOperator(filter.Operator), index);
                     }
-                    if ( filter.Field== "dmv_ReportExist" || filter.Field== "gplr_ReportExist"|| filter.Field == "oshugpz_ReportExist" || 
-                        filter.Field== "otsod_ReportExist" || filter.Field== "osp_ReportExist" || filter.Field== "otse_ReportExist"|| filter.Field== "otss_ReportExist" || filter.Field== "ushugpu_ReportExist"
-                        || filter.Field== "otvu_ReportExist" || filter.Field== "otu_ReportExist"||filter.Field== "to_ReportExist" )
+                    if (filter.Field == "NumML" || filter.Field == "dmv_ReportExist" || filter.Field == "gplr_ReportExist" || filter.Field == "oshugpz_ReportExist" ||
+                        filter.Field == "otsod_ReportExist" || filter.Field == "osp_ReportExist" || filter.Field == "otse_ReportExist" || filter.Field == "otss_ReportExist" || filter.Field == "ushugpu_ReportExist"
+                        || filter.Field == "otvu_ReportExist" || filter.Field == "otu_ReportExist" || filter.Field == "to_ReportExist")
                     {
                         parameters.Add(int.Parse(filter.Value));
-                        return string.Format("{0}{1}@{2}",
-                            filter.Field,
-                            ToLinqOperator(filter.Operator),
-                            index);
+                        return string.Format("{0}{1}@{2}", filter.Field, ToLinqOperator(filter.Operator), index);
                     }
                     parameters.Add(filter.Value);
-                    return string.Format("{0}{1}@{2}",
-                        filter.Field,
-                        ToLinqOperator(filter.Operator),
-                        index);
+                    return string.Format("{0}{1}@{2}", filter.Field, ToLinqOperator(filter.Operator), index);
                 case "startswith":
                     parameters.Add(filter.Value);
-                    return string.Format("{0}.StartsWith(" + "@{1})",
-                        filter.Field,
-                        index);
+                    return string.Format("{0}.StartsWith(" + "@{1})", filter.Field, index);
                 case "endswith":
                     parameters.Add(filter.Value);
-                    return string.Format("{0}.EndsWith(" + "@{1})",
-                        filter.Field,
-                        index);
+                    return string.Format("{0}.EndsWith(" + "@{1})", filter.Field, index);
                 case "contains":
                     parameters.Add(filter.Value);
-                    return string.Format("{0}.Contains(" + "@{1})",
-                        filter.Field,
-                        index);
+                    return string.Format("{0}.Contains(" + "@{1})", filter.Field, index);
                 default:
-                    throw new ArgumentException(
-                        "This operator is not yet supported for this Grid",
-                        filter.Operator);
+                    throw new ArgumentException("This operator is not yet supported for this Grid", filter.Operator);
             }
         }
 
